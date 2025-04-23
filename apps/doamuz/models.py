f402a2e0eb4f22dcc63bff1b22dea549ef6391b0
Yasmin Pereira Pasqua <13771997623@MUZAMBINHO.EDU.BR>
2025-04-23 15:45:02 -0300

    feat: implementar categoria

--- apps/doamuz/models.py
+++ apps/doamuz/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class categoria(models.Model):
+        cidade = models.CharField(max_length=100, verbose_name="Cidade")
+        tipo_doação = models.CharField(max_length=100, verbose_name="Tipo de Doação")
+        def __str__(self):
+            return f"{self.nome}"
+        class Meta:
+            verbose_name = "Categoria"
+            verbose_name_plural = "Categorias"
