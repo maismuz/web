634c534524076da53eeafc66566bba135c658c18
Yasmin Pasqua <yasminppasqua16@gmail.com>
2025-04-23 16:30:48 -0300

    feat: implementacao de doacoes

--- apps/doamuz/admin.py
+++ apps/doamuz/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(categoria)
+admin.site.register(Categoria)
+admin.site.register(Doacao)
--- apps/doamuz/models.py
+++ apps/doamuz/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class categoria(models.Model):
+class Categoria(models.Model):
         cidade = models.CharField(max_length=100, verbose_name="Cidade")
         tipo_doação = models.CharField(max_length=100, verbose_name="Tipo de Doação")
         def __str__(self):
@@ -9,3 +9,16 @@ class categoria(models.Model):
         class Meta:
             verbose_name = "Categoria"
             verbose_name_plural = "Categorias"
+
+class Doacao(models.Model):
+        titulo = models.CharField(max_length=200)
+        descricao = models.TextField(help_text="Descreva o que está sendo solicitado")
+        categoria = models.ForeignKey(Categoria, on_delete=models.SET_NULL, null=True)
+        quantidade_necessaria = models.PositiveIntegerField(null=True, blank=True)
+        data_criacao = models.DateTimeField(auto_now_add=True)
+        foi_atendido = models.BooleanField(default=False)
+        def __str__(self):
+            return f"{self.nome}"
+        class Meta:
+            verbose_name = "Doação"
+            verbose_name_plural = "Doações"
