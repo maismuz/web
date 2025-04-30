5a7570058a9f1fea4c242c019b013232c51ec039
henricaioo <12201000103@muz.ifsuldeminas.edu.br>
2025-04-30 14:08:34 -0300

    inicio

--- apps/covamuz/models.py
+++ apps/covamuz/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Cemiterio(models.Model):
+    nome = models.CharField(max_length=100, verbose_name="Nome do cemitério")
+    
+    def __str__(self):
+        return f"{self.nome}, {self.uf}"
+    class Meta:
+        verbose_name = "Cidade"
+        verbose_name_plural = "Cidades"
