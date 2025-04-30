99a6e2438c18faceb86fb66274010405652b97a7
Gabriel <gabriel.ribeiro@alunos.ifsuldeminas.edu.br>
2025-04-30 16:05:14 -0300

    feat: Front de publicacoes

--- config/urls.py
+++ config/urls.py
@@ -12,8 +12,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     # Rotas das demais aplicações
-    path("", include("apps.core.urls")),   
-     
+    path("", include("apps.core.urls")),
 ]
 # Configuração de arquivos de mídia
 if settings.DEBUG:
