7448778eef1cd63ae99866621291ad540c1c1ddb
Gulirovisk <lucinhogarciacaboverde@gmail.com>
2025-04-30 13:41:22 -0300

    Criação de tabela de serviços.

--- apps/contratamuz/models.py
+++ apps/contratamuz/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# Criação de modelos de contratos e curriculos para serviços autonomos ou contratação de empreasas;
+
+class Usuario(models.Model):
+    eh_empresa = models.BooleanField(default=False)
+    eh_prestador = models.BooleanField(default=False)
+    telefone = models.CharField(max_length=15, blank=True)
+    biografia = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.user.username
+class Servico(models.Model):
+    titulo = models.CharField(max_length=100)
+    descricao = models.TextField()
+    categoria = models.CharField(max_length=100)
+    criado_em = models.DateTimeField(auto_now_add=True)
+    link_contato = models.URLField(blank=True)
+
+    def __str__(self):
+        return self.titulo
