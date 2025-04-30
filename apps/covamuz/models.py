79631d49192d8285e6c6bc1f7cba4867a82060e7
henricaioo <12201000103@muz.ifsuldeminas.edu.br>
2025-04-30 14:22:11 -0300

    Criação da models dos cemitérios

--- apps/covamuz/models.py
+++ apps/covamuz/models.py
@@ -2,9 +2,10 @@ from django.db import models
 
 class Cemiterio(models.Model):
     nome = models.CharField(max_length=100, verbose_name="Nome do cemitério")
-    
+    cidade = models.CharField(max_length=100, verbose_name="Cidade do cemitério")
+
     def __str__(self):
-        return f"{self.nome}, {self.uf}"
+        return f"{self.nome}"
     class Meta:
-        verbose_name = "Cidade"
-        verbose_name_plural = "Cidades"
+        verbose_name = "Cemitério"
+        verbose_name_plural = "Cemitérios"
