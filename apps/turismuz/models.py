bf7774433ababd9e7fa4d3d3582b7042d40ba58f
Gabriel <gabriel.ribeiro@alunos.ifsuldeminas.edu.br>
2025-04-30 13:19:40 -0300

    feat: Adicionar imagem em publicacao

--- apps/turismuz/models.py
+++ apps/turismuz/models.py
@@ -144,7 +144,7 @@ class Publicacao(models.Model):
     titulo = models.CharField(max_length=255, verbose_name='Título da Publicação', help_text='Título que aparecerá no card de sua publicação.')
     texto_da_noticia = models.TextField(verbose_name = 'Texto da Publicação', help_text = 'Texto que aparecerá no corpo de sua publicação')
     data_de_publicacao = models.DateTimeField(auto_now_add = True, verbose_name='Data de Publicação')
-
+    imagem = models.ImageField(upload_to='imagens_publicacoes/', verbose_name='Imagem da Publicação', help_text='Imagem que aparecerá no card de sua publicação.')
 class Avaliacao(models.Model):
     nota = models.PositiveSmallIntegerField(
         verbose_name='Nota',
