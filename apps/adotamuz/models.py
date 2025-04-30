ece657940b56b29b0db68fb6c689546b9e542bd6
Lauralbertino <alblaura60@gmail.com>
2025-04-30 16:54:47 -0300

    gerenciar raças

--- apps/adotamuz/models.py
+++ apps/adotamuz/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from import_export import resources
+from .models import Raca
 
-# Create your models here.
+
+
+class RacaResource(resources.ModelResource):
+    class Meta:
+        model = Raca
+        skip_unchanged = True
+        report_skipped = True
+        fields = ('raca', 'caracteristica')
+
+
+@admin.register(Raca)
+
+class RacaAdmin(ImportExportModelAdmin):
+    resource_class = RacaResource
+    list_display = ('Raca','Caracteristicas')
+    list_filter = ('porte')
