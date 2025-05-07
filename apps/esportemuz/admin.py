18c91b444837a650f565e3eacbf8c21e7f2cfc2f
jotap1101 <joaopedroalves.j1101p@gmail.com>
2025-05-07 16:32:36 -0300

    Add ModalidadeAdmin to admin.py

--- apps/esportemuz/admin.py
+++ apps/esportemuz/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from esportemuz.models import *
 
 # Register your models here.
+class ModalidadeAdmin(admin.ModelAdmin):
+    list_display = ['id', 'nome']
+    list_per_page = 10
