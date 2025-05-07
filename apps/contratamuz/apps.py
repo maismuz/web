fcc9851174f96744275d552d3da0736d7b540df6
caiomaz <caiotmzferreira@gmail.com>
2025-05-07 15:58:44 -0300

    fix: atualizar o nome dos apps

--- apps/contratamuz/apps.py
+++ apps/contratamuz/apps.py
@@ -3,5 +3,5 @@ from django.apps import AppConfig
 
 class ContratamuzConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'contratamuz'
+    name = 'apps.contratamuz'
     verbose_name = 'ContrataMuz'
--- apps/muzeu/apps.py
+++ apps/muzeu/apps.py
@@ -3,5 +3,5 @@ from django.apps import AppConfig
 
 class MuzeuConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'muzeu'
+    name = 'apps.muzeu'
     verbose_name = 'Muzeu'
