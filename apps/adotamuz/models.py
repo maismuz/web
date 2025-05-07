76dcca5cdcceb6c693c38f208c802f5521d15dfa
Lauralbertino <alblaura60@gmail.com>
2025-05-07 16:35:08 -0300

    gerenciar raca

--- apps/adotamuz/models.py
+++ apps/adotamuz/models.py
@@ -10,6 +10,32 @@ from django.core.validators import RegexValidator
 def validar_nome(value):
     if not value.replace(' ', '').isalpha():
         raise ValidationError('O nome deve conter apenas letras e espaços.')
+    
+
+class Raca(models.Model):
+    
+    PORTE_CHOICES = [('pequeno', 'Pequeno'), ('medio', 'Médio'), ('grande', 'Grande')]
+    ESPECIE_CHOICES = [('cachorro', 'Cachorro'), ('gato', 'Gato')]
+
+    nome = models.CharField(max_length=100, unique=True, verbose_name='Nome')
+    descricao = models.TextField(blank=True, null=True, verbose_name='Descrição')
+    porte = models.CharField(max_length=10, choices=PORTE_CHOICES, default='medio', verbose_name='Porte')
+    especie = models.CharField(max_length=10, choices=ESPECIE_CHOICES, default='cachorro', verbose_name='Espécie')
+    data_cadastro = models.DateTimeField(auto_now_add=True, verbose_name='Data de Cadastro')
+
+    class Meta:
+        verbose_name = 'Raça'
+        verbose_name_plural = 'Raças'
+        ordering = ['nome']
+        unique_together = ('nome', 'especie')
+
+    def __str__(self):
+        return f'{self.nome} ({self.especie})'
+
+    def save(self, *args, **kwargs):
+        self.nome = self.nome.strip().capitalize()
+        self.full_clean()
+        super().save(*args, **kwargs)
 
 class Animal(models.Model):
     nome = models.CharField(
@@ -133,27 +159,3 @@ class Procedimento(models.Model):
         return f"{self.tipo.nome} ({self.tipo.animal.nome})"
 
 
-class Raca(models.Model):
-    
-    PORTE_CHOICES = [('pequeno', 'Pequeno'), ('medio', 'Médio'), ('grande', 'Grande')]
-    ESPECIE_CHOICES = [('cachorro', 'Cachorro'), ('gato', 'Gato')]
-
-    nome = models.CharField(max_length=100, unique=True, verbose_name='Nome')
-    descricao = models.TextField(blank=True, null=True, verbose_name='Descrição')
-    porte = models.CharField(max_length=10, choices=PORTE_CHOICES, default='medio', verbose_name='Porte')
-    especie = models.CharField(max_length=10, choices=ESPECIE_CHOICES, default='cachorro', verbose_name='Espécie')
-    data_cadastro = models.DateTimeField(auto_now_add=True, verbose_name='Data de Cadastro')
-
-    class Meta:
-        verbose_name = 'Raça'
-        verbose_name_plural = 'Raças'
-        ordering = ['nome']
-        unique_together = ('nome', 'especie')
-
-    def __str__(self):
-        return f'{self.nome} ({self.especie})'
-
-    def save(self, *args, **kwargs):
-        self.nome = self.nome.strip().capitalize()
-        self.full_clean()
-        super().save(*args, **kwargs)
