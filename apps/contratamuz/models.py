3cfcd70e81b49ecd8efd42909e8f2053c4212bf1
Gulirovisk <lucinhogarciacaboverde@gmail.com>
2025-05-07 14:08:55 -0300

    Adiciona campos de status e imagem aos modelos Candidatura, Serviço e Usuário, e atualiza opções de modelo

--- apps/contratamuz/models.py
+++ apps/contratamuz/models.py
@@ -24,7 +24,7 @@ class Servico(models.Model):
     descricao = models.TextField()
     categoria = models.CharField(max_length=100)
     criado_em = models.DateTimeField(auto_now_add=True)
-    link_contato = models.URLField(blank=True)
+    contato = models.CharField(max_length=15, blank=True)
     imagem = models.ImageField(upload_to='servicos/', blank=True, null=True)
 
     def __str__(self):
