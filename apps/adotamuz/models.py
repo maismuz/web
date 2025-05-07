c1b63eaf91b83e3d599f66f06410b5ab805852c9
Lauralbertino <alblaura60@gmail.com>
2025-05-07 16:36:16 -0300

    correcao exportacoes

--- apps/adotamuz/models.py
+++ apps/adotamuz/models.py
@@ -1,9 +1,4 @@
 from django.db import models
-
-# Create your models here.
-from django.db import models
-
-# Create your models here.
 from django.core.exceptions import ValidationError
 from django.core.validators import RegexValidator
 
