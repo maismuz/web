035fd1e075288d826deb6effb812aad27eadd685
henricaioo <12201000103@muz.ifsuldeminas.edu.br>
2025-05-07 16:54:27 -0300

    inicial(erro)

--- apps/covamuz/admin.py
+++ apps/covamuz/admin.py
@@ -4,3 +4,6 @@ from django.contrib import admin
 admin.site.register(Cemiterio)
 admin.site.register(AreaCemiterio)
 admin.site.register(Tumulo)
+admin.site.register(HorarioVisitacao)
+admin.site.register(Hora)
+admin.site.register(Dia)
