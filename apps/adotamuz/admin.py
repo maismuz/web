0f183bf2b53ebb864e1ca4b75921342b35ab6d71
fabiola.lima <fabiolalimacarvalho943@gmail.com>
2025-05-07 15:35:48 -0300

    feat: 156 adocao

--- apps/adotamuz/admin.py
+++ apps/adotamuz/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from import_export.admin import ImportExportModelAdmin
+from import_export import resources
+from .models import Animal
+
+class AnimalResource(resources.ModelResource):
+    class Meta:
+        model = Animal
+        skip_unchanged = True
+        report_skipped = True
+        fields = ('nome', 'porte', 'raca', 'cor', 'localizacao', 'foto', 'descricao')
+
+@admin.register(Animal)
+class AnimalAdmin(ImportExportModelAdmin):
+    resource_class = AnimalResource
+    list_display = ('nome', 'porte', 'cor', 'localizacao')
+    list_filter = ('porte', 'cor', 'localizacao')
+    search_fields = ('nome', 'raca', 'descricao', 'localizacao') 
+    fieldsets = (
+        ('Informações do Animal', {
+            'fields': ('nome', 'foto', 'cor', 'porte', 'raca', 'localizacao', 'descricao')
+        }),
+    )
