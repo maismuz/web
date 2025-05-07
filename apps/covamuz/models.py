de1116b17fa47fb3901c455b9bfa1c77fb33aa46
henricaioo <12201000103@muz.ifsuldeminas.edu.br>
2025-05-07 13:53:51 -0300

    criando classes

--- apps/covamuz/models.py
+++ apps/covamuz/models.py
@@ -9,3 +9,13 @@ class Cemiterio(models.Model):
     class Meta:
         verbose_name = "Cemitério"
         verbose_name_plural = "Cemitérios"
+
+class AreaCemiterio(models.Model):
+    nome = models.CharField(max_length=100, verbose_name="Nome do área")
+    Cemiterio = models.ForeignKey(Cemiterio)
+
+    def __str__(self):
+        return f"{self.nome}"
+    class Meta:
+        verbose_name = "Área do cemitério"
+        verbose_name_plural = "Áreas do cemitério"
