eceb5e2479594c62a793f4a2c3c8828713f48a37
caiomaz <caiotmzferreira@gmail.com>
2025-05-07 16:51:45 -0300

    feat: adicionar view de homepage e para os form base

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 
-def index(request):
+def homepage(request):
     """
     Função de view para renderizar a página inicial do site.
     """
-    return render(request, "index.html")
+    return render(request, "homepage.html")
+
+def base_forms(request):
+    """
+    Função de view para renderizar a página de formulários base.
+    """
+    return render(request, "base_forms.html")
