4460f28dd612ab971c6a4a664c5c43c0118b26ee
Luís Felipe Ricciardi Cerdeira <12461929696@MUZAMBINHO.EDU.BR>
2025-05-07 16:18:19 -0300

    gerenciar pessoas tumulos

--- apps/covamuz/models.py
+++ apps/covamuz/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Pessoa(models.Model):
+    nome = models.CharField(max_length=100, verbose_name="Nome Completo")
+    data_nasc = models.DateField(verbose_name="Data De Nascimento")
+    data_fale = models.DateField(verbose_name="Data De Falecimento")
+    #cemiterio = models.ForeignKey(Cemiterio, on_delete=models.PROTECT,verbose_name="Cemitério onde entá esterrado(a)")
+    #tumulo = models.ForeignKey(Tumulo, on_delete.PROTECT,verbose_name="Túmulo enterrado")
+    def __str__(self) :
+        return self.nome
+    class Meta:
+        verbose_name = "Pessoa"
+        verbose_name_plural = "Pessoas"
