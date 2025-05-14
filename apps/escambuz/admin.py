aa3242d33d813b18556caacd1129d489134bc608
Caio Tomaz <102072922+caiomaz@users.noreply.github.com>
2025-05-14 16:41:10 -0300

    Revert "[Feature] Gerenciar categoria de objeto (#203)"
    This reverts commit 25eb01636a6ae055aef31fc48fca38a6f28aa878.

--- apps/escambuz/admin.py
+++ apps/escambuz/admin.py
@@ -1,16 +1,3 @@
 from django.contrib import admin
-from .models import (Categoria, Objeto)
+
 # Register your models here.
-
-class CategoriaAdmin(admin.ModelAdmin):
-    list_display = ['nome', 'descricao']
-    search_fields = ['nome']
-
-admin.site.register(Categoria, CategoriaAdmin)
-
-class ObjetoAdmin(admin.ModelAdmin):
-    list_display = ['nome', 'tipo', 'preco', 'categoria', 'usuario', 'estado', 'data_cadastro']
-    search_fields = ['nome', 'descricao', 'usuario__username']
-    list_filter = ['tipo', 'categoria', 'estado']
-
-admin.site.register(Objeto, ObjetoAdmin)
--- apps/escambuz/models.py
+++ apps/escambuz/models.py
@@ -1,24 +1,3 @@
 from django.db import models
 
 # Create your models here.
-
-class Categoria(models.Model):
-    nome = models.CharField(max_length=100)
-    descricao = models.TextField(blank=True, null=True)
-
-    def __str__(self):
-        return self.nome
-    
-    class Objeto(models.Model):
-        TIPO_CHOICES = (('venda', 'Venda'), ('troca', 'Troca'), ('doacao', 'Doação'), )
-        nome = models.CharField(max_length=255)
-        descricao = models.TextField()
-        preco = models.DecimalField(max_digits=10, decimal_places=2)
-        tipo = models.CharField(max_length=20, choices=TIPO_CHOICES)
-        categoria = models.ForeignKey(Categoria, on_delete=models.SET_NULL, null=True, blank=True)
-        usuario = models.ForeignKey(User, on_delete=models.CASCADE)
-        estado = models.CharField(max_length=100)
-        data_cadastro = models.DateField(auto_now_add=True)
-
-    def __str__(self):
-        return self.nome
