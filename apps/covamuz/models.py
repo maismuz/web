083e6f31301a9d10b1ad27fc79014c156599183d
Luís Felipe Ricciardi Cerdeira <12461929696@MUZAMBINHO.EDU.BR>
2025-05-14 14:08:49 -0300

    feat: gerenciar pessoas os túmulos

--- apps/covamuz/models.py
+++ apps/covamuz/models.py
@@ -59,3 +59,15 @@ class Hora(models.Model):
     class Meta:
         verbose_name = "Hora"
         verbose_name_plural = "Horas"
+
+class Pessoa(models.Model):
+    nome = models.CharField(max_length=100, verbose_name="Nome Completo")
+    data_nasc = models.DateField(verbose_name="Data De Nascimento")
+    data_fale = models.DateField(verbose_name="Data De Falecimento")
+    cemiterio = models.ForeignKey(Cemiterio, on_delete=models.PROTECT,verbose_name="Cemitério onde entá esterrado(a)")
+    tumulo = models.ForeignKey(Tumulo, on_delete=models.PROTECT,verbose_name="Túmulo onde está enterrado")
+    def __str__(self) :
+        return self.nome
+    class Meta:
+        verbose_name = "Pessoa"
+        verbose_name_plural = "Pessoas"
