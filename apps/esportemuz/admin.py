6814c9a8845e43a246433205a2e6a607baf5767a
oguel <miguelcustodiopereira@gmail.com>
2025-05-14 13:54:46 -0300

    Update admin display and filters

--- apps/esportemuz/admin.py
+++ apps/esportemuz/admin.py
@@ -11,35 +11,29 @@ class TipoCampeonatoAdmin(admin.ModelAdmin):
     list_per_page = 10
 
 class CampeonatoAdmin(admin.ModelAdmin):
-    list_display = ['id', 'nome', 'modalidade', 'tipo_campeonato', 'data_inicio', 'data_fim']
+    list_display = ['id', 'nome', 'modalidade', 'tipo_campeonato', 'data_inicio', 'data_fim','equipes']
     list_per_page = 10
     search_fields = ['nome']
     list_filter = ['modalidade', 'tipo_campeonato']
 
 class EquipeAdmin(admin.ModelAdmin):
-    list_display = ['id', 'nome']
+    list_display = ['id', 'nome','escudo']
     list_per_page = 10
     search_fields = ['nome']
 
-class GrupoAdmin(admin.ModelAdmin):
-    list_display = ['id', 'nome', 'campeonato']
-    list_per_page = 10
-    search_fields = ['nome']
-    list_filter = ['campeonato']
-
-class StatusPartidaAdmin(admin.ModelAdmin):
+class LocalPartidaAdmin(admin.ModelAdmin):
     list_display = ['id', 'nome']
     list_per_page = 10
     search_fields = ['nome']
 
 class PartidaAdmin(admin.ModelAdmin):
-    list_display = ['id', 'campeonato', 'equipe1', 'equipe2', 'data_hora', 'status']
+    list_display = ['id', 'campeonato', 'equipe_mandante', 'equipe_visitante', 'data_hora', 'status', 'gols_mandante', 'gols_visitante', 'local','encerrada']
     list_per_page = 10
-    search_fields = ['campeonato__nome', 'equipe1__nome', 'equipe2__nome']
-    list_filter = ['campeonato', 'status']
+    search_fields = ['campeonato__nome', 'equipe_mandante__nome', 'equipe_visitante__nome']
+    list_filter = ['campeonato', 'status', 'local']
 
 class ClassificacaoAdmin(admin.ModelAdmin):
-    list_display = ['id', 'campeonato', 'equipe', 'pontos', 'vitorias', 'empates', 'derrotas', 'gols_pro', 'gols_contra']
+    list_display = ['id', 'campeonato', 'equipe', 'pontos', 'vitorias', 'empates', 'derrotas', 'gols_pro', 'gols_contra','saldo_gols']
     list_per_page = 10
     search_fields = ['campeonato__nome', 'equipe__nome']
     list_filter = ['campeonato']
