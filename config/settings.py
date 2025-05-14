d91bafab2816903851f04f6c95a115f87661b0d8
jotap1101 <joaopedroalves.j1101p@gmail.com>
2025-05-14 16:14:13 -0300

    Add additional date input format

--- config/settings.py
+++ config/settings.py
@@ -221,6 +221,7 @@ REST_FRAMEWORK = {
     ],
     'DATE_INPUT_FORMATS': [
         "%d/%m/%Y",
+        "%Y-%m-%d",
     ],
     'DEFAULT_FILTER_BACKENDS': [
         'django_filters.rest_framework.DjangoFilterBackend',
