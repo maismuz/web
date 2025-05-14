d2c8d556efa8563d67c05338b5a54a1a819f3954
PabloFernan <pablodgfernandes2005@gmail.com>
2025-05-14 14:31:36 -0300

    feat: implementar models e admin  denuncias de irregulares

--- apps/escambuz/admin.py
+++ apps/escambuz/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Oferta, DenunciaIrregular
 
-# Register your models here.
+
+@admin.register(Oferta)
+class OfertaAdmin(admin.ModelAdmin):
+    list_display = ['titulo', 'preco', 'data_criacao']
+    search_fields = ['titulo']
+    list_filter = ['data_criacao']
+
+
+@admin.register(DenunciaIrregular)
+class DenunciaIrregularAdmin(admin.ModelAdmin):
+    list_display = ['id', 'usuario', 'oferta', 'motivo_da_denuncia', 'status', 'data_denuncia']
+    list_filter = ['status', 'motivo_da_denuncia', 'data_denuncia']
+    search_fields = ['usuario__username', 'oferta__titulo']
+    autocomplete_fields = ['usuario', 'oferta']
+    ordering = ['-data_denuncia']
