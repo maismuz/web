0705528290f9e7bcd0f37640a9e7a052a8f82e7c
AyaneCristina27 <ayane.rosa@alunos.ifsuldeminas.edu.br>
2025-05-14 15:57:38 -0300

    feat: gerenciar busca de denuncias

--- apps/reclamemuz/admin.py
+++ apps/reclamemuz/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from import_export.admin import ImportExportModelAdmin
 from import_export import resources
-from .models import Denuncia
+from .models import Denuncia, Comentario, BuscaDenuncia
+
 
 class DenunciaResource(resources.ModelResource):
     class Meta:
@@ -18,3 +19,28 @@ class DenunciaAdmin(ImportExportModelAdmin):
     list_display = ('id_denuncia', 'titulo', 'categoria', 'status', 'data_ocorrencia', 'id_usuario')
     list_filter = ('categoria', 'status', 'data_ocorrencia')
     search_fields = ('titulo', 'descricao', 'endereco', 'id_usuario__username')
+
+@admin.register(Comentario)
+class ComentarioAdmin(admin.ModelAdmin):
+    list_display = ('id', 'id_usuario', 'id_denuncia', 'data_hora', 'resumo_texto')
+    list_filter = ('data_hora', 'id_usuario', 'id_denuncia')
+    search_fields = ('texto', 'id_usuario__username', 'id_denuncia__id')
+    date_hierarchy = 'data_hora'
+    ordering = ('-data_hora',)
+
+    def resumo_texto(self, obj):
+        return (obj.texto[:50] + '...') if len(obj.texto) > 50 else obj.texto
+    resumo_texto.short_description = 'Texto (resumo)'
+
+    from .models import Denuncia, Comentario, BuscaDenuncia
+
+@admin.register(BuscaDenuncia)
+class BuscaDenunciaAdmin(admin.ModelAdmin):
+    list_display = ('id', 'usuario', 'termo', 'categoria', 'status', 'bairro', 'data_inicial', 'data_final', 'data_busca')
+    list_filter = ('categoria', 'status', 'bairro', 'data_busca')
+    search_fields = ('termo', 'usuario__username', 'bairro')
+    date_hierarchy = 'data_busca'
+    ordering = ('-data_busca',)
+
+
+    
