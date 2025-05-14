8edde4e23f86daebae8d901e75c6bd47b1be7f52
AyaneCristina27 <ayane.rosa@alunos.ifsuldeminas.edu.br>
2025-05-14 16:53:51 -0300

    [Feature] Gerenciar busca de denuncias (#294)

--- apps/reclamemuz/models.py
+++ apps/reclamemuz/models.py
@@ -31,7 +31,6 @@ class Denuncia(models.Model):
         return f"{self.titulo} - {self.get_status_display()}"
 
 #Gerenciar comentários nas denúncias
-
 class Comentario(models.Model):
     texto = models.TextField()
     data_hora = models.DateTimeField(auto_now_add=True)
@@ -40,3 +39,21 @@ class Comentario(models.Model):
 
     def __str__(self):
         return f"Comentário de {self.id_usuario.username} em {self.data_hora.strftime('%d/%m/%Y %H:%M')}"
+    
+#Gerenciar Busca e filtros de denúncias
+from django.db import models
+from django.contrib.auth.models import User
+
+class BuscaDenuncia(models.Model):
+    usuario = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    termo = models.CharField("Termo de busca", max_length=255)
+    categoria = models.CharField("Categoria filtrada", max_length=20, blank=True, null=True)
+    status = models.CharField("Status filtrado", max_length=10, blank=True, null=True)
+    data_inicial = models.DateField("Data inicial", blank=True, null=True)
+    data_final = models.DateField("Data final", blank=True, null=True)
+    bairro = models.CharField("Bairro filtrado", max_length=100, blank=True, null=True)
+    data_busca = models.DateTimeField("Momento da busca", auto_now_add=True)
+
+    def __str__(self):
+        return f"Busca por '{self.termo}' em {self.data_busca.strftime('%d/%m/%Y %H:%M')}"
+
