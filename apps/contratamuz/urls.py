bf6ffcc56f66dffb5a6babde8b333728251ecf9a
Gulirovisk <lucinhogarciacaboverde@gmail.com>
2025-05-21 16:51:37 -0300

    [Feature] Atualizar página inicial e adicionar listagem de serviços

--- apps/contratamuz/urls.py
+++ apps/contratamuz/urls.py
@@ -2,6 +2,7 @@ from apps.contratamuz import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.inicial, name='inicial'),
+    path('', views.inicial, name='index '),
     path('vagas/', views.listar_vagas, name='listar_vagas'),
+    path('servicos/', views.listar_servicos, name='listar_servicos'),
 ]
--- apps/contratamuz/views.py
+++ apps/contratamuz/views.py
@@ -2,9 +2,13 @@ from django.shortcuts import render
 from apps.contratamuz.models import *
 
 def inicial(request):
-    return render(request, 'inicial.html')
+    return render(request, 'contratamuz/index.html')
 
 def listar_vagas(request):
     vagas = VagaEmprego.objects.filter(ativa=True).order_by('-criado_em')
-    return render(request, 'listar_vagas.html', {'vagas': vagas})
+    return render(request, 'contratamuz/listar_vagas.html', {'vagas': vagas})
+
+def listar_servicos(request):
+    servicos = Servico.objects.all().order_by('-criado_em')
+    return render(request, 'contratamuz/listar_servicos.html', {'servicos': servicos})
 
