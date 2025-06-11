f5097a322470fdcad0f9dbabd55f702d924a5d31
Gabriel <gabriel.ribeiro@alunos.ifsuldeminas.edu.br>
2025-06-11 16:29:03 -0300

    Ajustando nav bar

--- apps/turismuz/urls.py
+++ apps/turismuz/urls.py
@@ -6,7 +6,6 @@ urlpatterns = [
     path('estabelecimentos/', views.estabelecimentos, name='estabelecimentos'),
     path('estabelecimento/<int:pk>/', views.estabelecimento_detail, name='estabelecimento_detail'),
     path('guias/', views.guias, name='guias'),
-    path('publicacoes/', views.publicacoes, name='publicacoes'),
     path('publicacoes/<int:pk>/', views.publicacao_detail, name='publicacao_detail'),
     path('guia/<int:pk>/', views.guia_detail, name='guia_detail'),
 ]
