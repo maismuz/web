3a1798da52c6adc1330bc5984d8eebdb33d9b65b
AyaneCristina27 <ayane.rosa@alunos.ifsuldeminas.edu.br>
2025-06-11 16:00:39 -0300

    feat:criação da página home

--- apps/reclamemuz/views.py
+++ apps/reclamemuz/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import Denuncia
 
-# Create your views here.
+def index(request):
+    # denuncias_recentes = Denuncia.objects.order_by('-data_ocorrencia')[:5]
+    # total_denuncias = Denuncia.objects.count()
+    # total_pendentes = Denuncia.objects.filter(status='pendente').count()
+    # total_resolvidas = Denuncia.objects.filter(status='resolvido').count()
+
+    # contexto = {
+    #     'denuncias_recentes': denuncias_recentes,
+    #     'total_denuncias': total_denuncias,
+    #     'total_pendentes': total_pendentes,
+    #     'total_resolvidas': total_resolvidas,
+    # }
+
+    return render(request, 'homereclamemuz.html')
