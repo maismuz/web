6c4b66449b52ea5b73f8d75bbf112bd4ffe4e3bc
Alice Barros Pereira <103052000+AliceBPereira@users.noreply.github.com>
2025-07-09 05:29:14 -0300

    feat: create form denuncia

--- apps/reclamemuz/urls.py
+++ apps/reclamemuz/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
+app_name = 'reclamemuz'
 
 urlpatterns = [
     path('home', views.index, name='index'),
+    path('denunciar/', views.denuncias, name='denunciar'),
+    path('denuncia-sucesso/', views.denuncia_sucesso, name='denuncia_sucesso'),
 ]
--- apps/reclamemuz/views.py
+++ apps/reclamemuz/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Denuncia
+from .forms import DenunciaForm
 
 def index(request):
     # denuncias_recentes = Denuncia.objects.order_by('-data_ocorrencia')[:5]
@@ -15,3 +16,21 @@ def index(request):
     # }
 
     return render(request, 'homereclamemuz.html')
+
+def denuncias(request):
+    # REMOVIDO: @login_required e toda a lógica de usuário
+    if request.method == 'POST':
+        form = DenunciaForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save() # Simplesmente salva o formulário no banco de dados
+            return redirect('denuncia_sucesso') # Redireciona para uma página de sucesso
+    else:
+        form = DenunciaForm()
+        
+    return render(request, 'forms_Denuncia.html', {'form': form})
+
+def denuncia_sucesso(request):
+    """
+    View para a página de agradecimento após enviar uma denúncia.
+    """
+    return render(request, 'denuncia_sucesso.html')
