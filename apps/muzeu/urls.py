f58ef5dcf994d9bf47cac5536fe6f3be1157ff20
lcaruzo007 <lucas.caruzo@alunos.ifsuldeminas.edu.br>
2025-07-09 16:46:59 -0300

    feat:imagens socorro

--- apps/muzeu/urls.py
+++ apps/muzeu/urls.py
@@ -5,13 +5,13 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
-    path('item-acervo/', ItemAcervoView.as_view(), name='item_acervo'),
-    path('patrimonio/', PatrimonioView.as_view(), name='patrimonio'),
+    path('item-acervo/', ItemAcervoView.as_view(), name='item-acervo'),
+    path('patrimonio/<int:pk>', PatrimonioView.as_view(), name='patrimonio'),
     path('documento-historico/', DocumentoHistoricoView.as_view(), name='documento_historico'),
     path('form-itens-acervo/', FormItensAcervoView.as_view(), name='form_itens_acervo'),
     path('form-patrimonio/', FormPatrimonioView.as_view(), name='form_patrimonio'),
     path('form-documento-historico/', FormDocumentoHistoricoView.as_view(), name='form_documento_historico'),
-    path('lista-item/', ListaItemView.as_view(), name='lista_item'),
+    path('lista_item/', ListaItemView.as_view(), name='lista_item'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
