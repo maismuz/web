c6a82f3ee7fcfd35fbe369b0d18f9b91b9980aa5
Pablo Fernandes <131775404+PabloFernan@users.noreply.github.com>
2025-07-16 13:35:48 -0300

    feat: front-end da tela inicial (#403)
    Co-authored-by: Caio Tomaz <102072922+caiomaz@users.noreply.github.com>

--- apps/escambuz/views.py
+++ apps/escambuz/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Categoria, Objeto
 
-# Create your views here.
+def index(request):
+
+    categorias = Categoria.objects.all()
+    
+    objetos_recentes = Objeto.objects.order_by('-data_cadastro')[:9]
+    
+    context = {
+        'categorias': categorias,
+        'objetos': objetos_recentes
+    }
+    
+
+    return render(request, 'escambuz/index.html', context)
