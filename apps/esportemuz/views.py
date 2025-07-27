1c5684064bc84914a5de20f0458cb7550435ecf1
jotap1101 <jotap1101.joaopedro@gmail.com>
2025-07-27 14:56:35 -0300

    Remove duplicate json import in views.py

--- apps/esportemuz/views.py
+++ apps/esportemuz/views.py
@@ -1,3 +1,4 @@
+import json
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib import messages
 from django.http import JsonResponse
@@ -6,8 +7,6 @@ from django.views.decorators.csrf import csrf_exempt
 from django.core.paginator import Paginator
 from django.db.models import Q
 from django.contrib.auth.decorators import login_required
-import json
-
 from apps.esportemuz.models import Campeonato, Equipe, Participacao, Rodada, Partida, Classificacao
 from apps.esportemuz.services import CampeonatoService, ClassificacaoService, PartidaService, RodadaService
 from apps.esportemuz.forms import CampeonatoForm, EquipeForm, RodadaForm, PartidaManualForm
